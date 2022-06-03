9b439a6350f5fcd8c824d0d64b5f9174f54d3221
Ivaney Vieira <ivaneyvieira@gmail.com>
2022-06-03 05:53:56 -0300

    Planilha dos paineis com promoção

--- src/main/kotlin/br/com/astrosoft/promocao/view/promocao/TabAbstractPromocao.kt
+++ src/main/kotlin/br/com/astrosoft/promocao/view/promocao/TabAbstractPromocao.kt
@@ -5,6 +5,7 @@ import br.com.astrosoft.framework.view.TabPanelGrid
 import br.com.astrosoft.framework.view.shiftSelect
 import br.com.astrosoft.promocao.model.beans.FiltroPrecoPromocao
 import br.com.astrosoft.promocao.model.beans.PrecoPromocao
+import br.com.astrosoft.promocao.model.planilhas.PlanilhaPromocao
 import br.com.astrosoft.promocao.view.promocao.columns.NotaNddViewColumns.promocaoCentroLucro
 import br.com.astrosoft.promocao.view.promocao.columns.NotaNddViewColumns.promocaoCodigo
 import br.com.astrosoft.promocao.view.promocao.columns.NotaNddViewColumns.promocaoDesconto
@@ -17,12 +18,20 @@ import br.com.astrosoft.promocao.view.promocao.columns.NotaNddViewColumns.promoc
 import br.com.astrosoft.promocao.view.promocao.columns.NotaNddViewColumns.promocaoVendno
 import br.com.astrosoft.promocao.viewmodel.promocao.*
 import com.github.mvysny.karibudsl.v10.integerField
+import com.github.mvysny.karibudsl.v10.tooltip
+import com.vaadin.flow.component.HasComponents
+import com.vaadin.flow.component.button.ButtonVariant
 import com.vaadin.flow.component.grid.Grid
 import com.vaadin.flow.component.grid.Grid.SelectionMode.MULTI
 import com.vaadin.flow.component.grid.Grid.SelectionMode.SINGLE
+import com.vaadin.flow.component.icon.VaadinIcon
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout
 import com.vaadin.flow.component.textfield.IntegerField
 import com.vaadin.flow.data.value.ValueChangeMode.TIMEOUT
+import org.vaadin.stefan.LazyDownloadButton
+import java.io.ByteArrayInputStream
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
 
 abstract class TabAbstractPromocao<T : ITabAbstractPromocaoViewModel>(open val viewModel: TabAbstractPromocaoViewModel<T>) :
         TabPanelGrid<PrecoPromocao>(PrecoPromocao::class), ITabAbstractPromocaoViewModel {
@@ -68,7 +77,27 @@ abstract class TabAbstractPromocao<T : ITabAbstractPromocaoViewModel>(open val v
       }
     }
 
+    this.downloadExcel()
+
     addAditionaisFields()
+  }
+
+  private fun HasComponents.downloadExcel() {
+    val button = LazyDownloadButton(VaadinIcon.TABLE.create(), { filename() }, {
+      val planilha = PlanilhaPromocao()
+      val bytes = planilha.grava(listBeans())
+      ByteArrayInputStream(bytes)
+    })
+    button.addThemeVariants(ButtonVariant.LUMO_SMALL)
+    button.tooltip = "Salva a planilha"
+    add(button)
+  }
+
+  private fun filename(): String {
+    val sdf = DateTimeFormatter.ofPattern("yyMMddHHmmss")
+    val textTime = LocalDateTime.now().format(sdf)
+    val filename = "promocao$textTime.xlsx"
+    return filename
   }
 
   protected abstract fun HorizontalLayout.addAditionaisFields()
